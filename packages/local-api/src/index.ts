c1a6a03621f329001cfec29c9647b784ae694eb0
Build Pipeline <build.pipeline@softphone.it>
2021-12-16 00:14:20 +0100

    update(code): improve log

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { RequestHandler } from "express";
 import path from "path";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import { createCellsRouter } from "./routes/cells";
@@ -12,7 +12,7 @@ export const serve = (
   const app = express();
 
   const cellsRouter = createCellsRouter(filename, dir);
-  
+
   app.use(cellsRouter);
 
   if (useProxy) {
@@ -24,14 +24,25 @@ export const serve = (
       })
     );
   } else {
-    
-    const packagePath = require.resolve(
-      "@jscript-notebook/local-client/dist/index.html"
-    );
-    
-    console.log( '@jscript-notebook path', packagePath)
 
-    app.use( express.static( path.dirname(packagePath) ) );
+    const packageModulePath = (module: string) => {
+      let _path = require.resolve(module)
+      console.log(`${module} path: ${_path}`)
+      return path.dirname(_path)
+    }
+
+    app.use(express.static(packageModulePath("@jscript-notebook/local-client/dist/index.html")));
+
+    const local_pkg_path = packageModulePath('@jscript-notebook/local-pkg/README.md')
+
+    // LOG STATIC REQUEST    
+    const log:RequestHandler =  (req, res, next) => {
+      const { url, path: routePath } = req;
+      console.log(url, routePath);
+      next();
+    }
+    app.use('/local', log, express.static(path.join(local_pkg_path, 'node_modules')))
+
   }
 
   return new Promise<void>((resolve, reject) => {
