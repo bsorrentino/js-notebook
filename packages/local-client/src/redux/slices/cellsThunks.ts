3d650f0e64b083dc1ceec48c60813ff5736d2fc1
bsorrentino <bartolomeo.sorrentino@gmail.com>
2022-02-07 21:40:51 +0100

    #1 - add pouchdb basic usage

--- packages/local-client/src/redux/slices/cellsThunks.ts
+++ packages/local-client/src/redux/slices/cellsThunks.ts
@@ -2,6 +2,14 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { Cell } from "../cell";
 import axios from "axios";
 import { RootState } from "../store";
+import PouchDB from 'pouchdb'
+
+const db = new PouchDB<Cell>( 'jsnotebook' )
+
+const info = await db.info()
+
+console.log( 'database info', info)
+
 
 export const fetchCells = createAsyncThunk<
   Cell[],
@@ -9,9 +17,13 @@ export const fetchCells = createAsyncThunk<
   { rejectValue: string }
 >("cells/fetchCells", async (_, thunkAPI) => {
   try {
+    
+    const dataFormDb = await db.allDocs( { include_docs: true } )
+    console.log( 'dataFormDb #', dataFormDb.total_rows ) 
+    
     const { data }: { data: Cell[] } = await axios.get("/cells");
     return data;
-  } catch (error) {
+  } catch (error:any) {
     thunkAPI.rejectWithValue(error.message);
   }
   return [];
@@ -25,8 +37,10 @@ export const saveCells = createAsyncThunk<
   const { data, order } = getState().cells;
   const cells = order.map((id) => data[id]);
   try {
+    const res = await db.bulkDocs( cells )
+    console.log( 'bulkDocs', res )
     await axios.post("/cells", { cells });
-  } catch (error) {
+  } catch (error:any) {
     rejectWithValue(error.message);
   }
 });
