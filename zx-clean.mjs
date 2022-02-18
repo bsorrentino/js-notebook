1eec87c21f3aa542c361ff0442d86292f50c5286
bsorrentino <bartolomeo.sorrentino@gmail.com>
2022-02-18 19:43:51 +0100

    build(clean): add cleanall npm script

--- zx-clean.mjs
+++ zx-clean.mjs
@@ -3,3 +3,6 @@ import 'zx/globals'
 const cache_path = path.join( 'packages', 'local-client', '.parcel-cache' )
 await fs.remove( cache_path )
 
+if( argv.all ) {
+    await fs.remove( 'node_modules' )
+}
