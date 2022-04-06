7e81a42cf251a2b228b4ac46f26cbb70e6b1f666
bsorrentino <bartolomeo.sorrentino@gmail.com>
2022-04-06 18:29:57 +0200

    feat(server): start implementing service to emit ts declaration from source

---
+++ packages/server/src/routes/tsc.ts
@@ -0,0 +1,46 @@
+import path from "path";
+import ts from "typescript";
+
+export const createTSCsRouter = (dir: string) => {
+
+}
+
+/**
+ * 
+ * @link https://github.com/microsoft/TypeScript/wiki/Using-the-Compiler-API#getting-the-dts-from-a-javascript-file
+ * @param options 
+ * @param fileNames 
+ */
+
+export function generateDTS( fileName: string, options: ts.CompilerOptions|undefined): string|undefined {
+
+  const compileOptions : ts.CompilerOptions = {
+    ...options,
+    declaration: true,
+    emitDeclarationOnly: true
+  }
+
+  const createdFiles:Record<string,string> = {}
+
+  const host = ts.createCompilerHost(compileOptions);
+  host.writeFile = 
+    (fileName: string, contents: string) => createdFiles[fileName] = contents
+
+  const file = path.normalize( fileName )
+
+  const program = ts.createProgram( [file], compileOptions, host );
+
+  const emitResult = program.emit()
+
+  console.log( createdFiles )
+
+  if( !emitResult.emitSkipped ) {
+    
+    const outputPropName = path.join( path.dirname(file), path.basename(file).replace( /\.(js|ts)$/, '.d.ts') )
+
+    console.log( outputPropName )
+    
+    return createdFiles[outputPropName]
+  
+  }
+}
