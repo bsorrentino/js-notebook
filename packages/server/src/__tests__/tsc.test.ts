3802d6844803c5bb46e6e973a2fcd2b1c272dd5d
bsorrentino <bartolomeo.sorrentino@gmail.com>
2022-04-07 18:10:40 +0200

    feat: emit ts declaration - implementation refinements

---
+++ packages/server/src/__tests__/tsc.test.ts
@@ -0,0 +1,19 @@
+/// <reference types="@types/jest" />
+
+import ts from 'typescript'
+import { generateDTS } from '../tsc'
+
+describe( 'typescipt compiler test', () => {
+    
+    it( 'generate declaration', () => {
+
+        const result = generateDTS( 'src/__tests__/file1.ts', {
+            target: ts.ScriptTarget.ES5,
+            module: ts.ModuleKind.CommonJS,
+        })
+        expect( result ).toBeDefined()
+        expect( result ).not.toBeNull()
+
+    })
+
+})
