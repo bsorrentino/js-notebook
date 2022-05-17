78ff4045cd5fa1771fb2e43d031511d24a87373e
bsorrentino <bartolomeo.sorrentino@gmail.com>
2022-05-17 12:24:27 +0200

    refactor(logger): update logging configuration

--- packages/client/src/logger-init.ts
+++ packages/client/src/logger-init.ts
@@ -1,8 +1,9 @@
 import { initLoggers, LogLevel } from "@bsorrentino/jsnotebook-logger";
 
 initLoggers( LogLevel.info, [
-    { name: 'CodeCell', level: LogLevel.trace },
-    { name: 'monaco-editor-hook', level: LogLevel.debug },
-    { name: 'hooks', level: LogLevel.info },
+    { name: 'CodeCell',             level: LogLevel.info },
+    { name: 'monaco-editor-hook',   level: LogLevel.info },
+    { name: 'hooks',                level: LogLevel.info },
+    { name: 'unpkg-path-plugin',    level: LogLevel.info }
 
 ])  
